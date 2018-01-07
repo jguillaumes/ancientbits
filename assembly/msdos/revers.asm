d77ae9f76ac5a940fca01ac9b1b9d0713d004c2a
Jordi Guillaumes Pons <jguillaumes@gmail.com>
2018-01-07 15:06:22 +0100

    - msdos: translate comments to English

--- assembly/msdos/revers.asm
+++ assembly/msdos/revers.asm
@@ -6,79 +6,79 @@ BUFSIZ  EQU 256
 		.data
 
 hello   db  'Reverse program - MSDOS',0dh,0ah,'$'
-prompt  db  'Entra una linia:   ','$'
-inver   db  0dh,0ah,'Linia invertida:   ','$'
-adeu    db  0dh,0ah,'Finalitzat'
+prompt  db          'Enter a text line:  ','$'
+inver   db  0dh,0ah,'Inverted line:      ','$'
+bye    db  0dh,0ah,'Finished'
 crlf    db  0dh,0ah,'$'
 buffin  db  255
 numch   db  0
 bufchr  db  BUFSIZ dup(' ')
 bufout  db  BUFSIZ dup(' ')
-		
+
 		.code
-  
+
 main    proc
-		mov ax, seg hello       ; C…rrega segments de dades
+		mov ax, seg hello       ; Setup data segment (DS and ES)
 		mov ds, ax
 		mov es, ax
 
-		lea dx, hello           ; Mostrar missatge inicial
-		mov ah, 09h
-		int 21h      
-
-getlin: lea dx, prompt          ; Mostrar prompt
+		lea dx, hello           ; Show welcome message
 		mov ah, 09h
 		int 21h
 
-		lea dx, buffin          ; Llegir linia d'entrada
-		mov ah, 0ch             ; Netejar buffer STDIN
-		mov al, 0ah             ; Llegir amb buffer
+getlin: lea dx, prompt      		; Show prompt
+		mov ah, 09h
 		int 21h
-		mov al, byte ptr [numch]; AL: Nombre de caracters
-		cmp al, 0               ; Buffer buit?
-		je  final               ; Si: acabar
 
-		lea dx,inver
+		lea dx, buffin          ; Input text line
+		mov ah, 0ch             ; Clear STDIN buffer
+		mov al, 0ah             ; Buffered read DOS function
+		int 21h
+		mov al, byte ptr [numch]; AL: Number of characters
+		cmp al, 0               ; Is the buffer empty?
+		je  final               ; Yes: finish
+
+		lea dx,inver		; Display "Inverted..." text 
 		mov ah,09h
 		int 21h
 
-		lea ax,bufchr           ; AX => Inici buffer entrada
+		lea ax,bufchr           ; AX => Start of input buffer
 		sub cx,cx               ; CX => Zero
-		mov cl,byte ptr [numch] ; CX => Bytes a buffer
-		add ax,cx               ; AX => Final buffer entrada+1
-		dec ax                  ; AX => Final cadena entrada
-		mov si,ax               ; SI => Final buffer entrada
-		lea di,bufout           ; DI => Inici buffer sortida
+		mov cl,byte ptr [numch] ; CX => Number of bytes in buffer
+		add ax,cx               ; AX => End of input text + 1
+		dec ax                  ; AX => End of input text
+		mov si,ax               ; SI => End of input text
+		lea di,bufout           ; DI => Start of output buffer
 
 theloop:
-		std                     ; Sentit SI: Descendent
-		lodsb                   ; Carregar byte a AL
-		cld                     ; Sentit DI: Ascendent
-		stosb                   ; Desar byte a buffer
-		loop theloop            ; Saltar segons comptador CX
-		mov cx,3                ; Apuntar a sequencia CR+LF+$ 
-		lea si,crlf             ;
-		rep movsb               ; I afegir-la al buffer
+		std                     ; change direction: decrement
+		lodsb                   ; Load byte from DS:SI, decrement SI
+		cld                     ; change direction: increment
+		stosb                   ; Store byte at ES:DI, increment DI
+		loop theloop            ; Check CX and loop if not zero
+		mov cx,3                ; Prepare to move 3 more bytes (CR,LF,'$')
+		lea si,crlf             ; DS:SI => CR+LF+'$'
+		rep movsb               ; Move to ES:DS (append to output buffer)
 
-		lea dx, bufout          ; Emetre el buffer de sortida...
-		mov ah, 09h
-		int 21h
-		
-		lea dx,crlf             ; Emetre un altre salt de linia
+		lea dx, bufout          ; Display output buffer (inverted text)
 		mov ah, 09h
 		int 21h
 
-		jmp getlin              ; Processar una altra linia
-
-
-final:  lea dx, adeu
+		lea dx,crlf             ; New line...
 		mov ah, 09h
 		int 21h
 
-		mov ax, 4C00h
+		jmp getlin              ; Get one more line to process
+
+
+final:  	lea dx, bye		; Display goodbye message
+		mov ah, 09h
+		int 21h
+
+		mov ax, 4C00h		; End program, code=00 (ah=4Ch, al=00h)
 		int 21h
 
 		ret
 main    endp
-	
+
 		end main
